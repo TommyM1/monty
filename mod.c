70ff5f683fe9516f897600502a967fdd726d1590
TommyM1 <60845659+TommyM1@users.noreply.github.com>
2023-11-19 09:40:06 +0100

    Add files via upload

---
+++ mod.c
@@ -0,0 +1,43 @@
+#include "monty.h"
+
+/**
+ * f_mod - computes the rest of the division of the second
+ * top element of the stack by the top element of the stack
+ * @head: stack head
+ * @counter: line_number
+ * Return: result
+*/
+
+void f_mod(stack_t **head, unsigned int counter)
+{
+	stack_t *hd;
+	int len = 0, aux;
+
+	hd = *head;
+	while (hd)
+	{
+		hd = hd->next;
+		len++;
+	}
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't mod, stack too short\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	hd = *head;
+	if (hd->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", counter);
+		fclose(chain.file);
+		free(chain.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	aux = hd->next->n % hd->n;
+	hd->next->n = aux;
+	*head = hd->next;
+	free(hd);
+}
